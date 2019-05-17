5f79eb8ae1966beed192a5ddcddd50523bdc558f
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-05-17 22:37:53 +0300

    lab 3 added complex digit class

---
+++ digit_number/digit_number/ComplexDigit.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace digit_number {
+    public struct ComplexDigit {
+        private double real;
+        private double imagine;
+
+        public ComplexDigit(double r, double im) {
+            this.real = r;
+            this.imagine = im;
+        }
+
+        public static ComplexDigit operator + (ComplexDigit lhs, ComplexDigit rhs) {
+            double r = lhs.real + rhs.real;
+            double im= lhs.imagine + rhs.imagine;
+
+            return new ComplexDigit(r, im);
+        }
+
+        public static ComplexDigit operator - (ComplexDigit lhs, ComplexDigit rhs) {
+            double r = lhs.real - rhs.real;
+            double im = lhs.imagine - rhs.imagine;
+
+            return new ComplexDigit(r, im);
+        }
+
+        public static ComplexDigit operator *(ComplexDigit lhs, ComplexDigit rhs) {
+            double r = lhs.real * rhs.real;
+            double im = lhs.imagine * rhs.imagine;
+
+            return new ComplexDigit(r, im);
+        }
+
+        public static ComplexDigit operator /(ComplexDigit lhs, ComplexDigit rhs) {
+            double r = lhs.real / rhs.real;
+            double im = lhs.imagine / rhs.imagine;
+
+            return new ComplexDigit(r, im);
+        }
+
+        public static bool operator == (ComplexDigit lhs, ComplexDigit rhs) {
+            bool br = lhs.real == rhs.real;
+            bool bim = lhs.imagine == rhs.imagine;
+
+            return (br== true && bim==true);
+        }
+
+        public static bool operator !=(ComplexDigit lhs, ComplexDigit rhs) {
+            //bool br = lhs.real == rhs.real;
+            //bool bim = lhs.imagine == rhs.imagine;
+
+            //return (br != true || bim != true);
+            return !(lhs == rhs);
+        }
+
+        public string print() {
+            return "Целая часть: {real} Мнимая часть: {imagine}";
+        }
+    }
+
+
+}
--- digit_number/digit_number/Program.cs
+++ digit_number/digit_number/Program.cs
@@ -3,7 +3,15 @@
 namespace digit_number {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Комплексные числа");
+
+            var a = new ComplexDigit(4.5, 2.0);
+            var b = new ComplexDigit(7.5, 1.15);
+
+            var c = a + b;
+            Console.WriteLine(a.print() + " + " + b.print() + " = " + c.print());
+
+            Console.ReadLine();
         }
     }
 }
