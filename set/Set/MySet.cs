e51089e564955ae3690c268f83c4c5ef64c7d3f9
Grigorii Ilin <37904148+Grigorii-Ilin@users.noreply.github.com>
2019-05-18 09:26:41 +0300

    lab 4 class set

---
+++ set/Set/MySet.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Set {
+    public class MySet {
+        public List<int> UniqueValues { get; private set; }
+        //private List<int> uniqueValues;
+
+        public MySet(int[] values) {
+            foreach (var value in values) {
+                if (!UniqueValues.Contains(value)) {
+                    UniqueValues.Add(value);
+                }
+            }
+        }
+
+        public MySet() { }
+
+        public int GetCardinality() {
+            return UniqueValues.Count;
+        }
+
+        public void AddValue(int value) {
+            if (!UniqueValues.Contains(value)) {
+                UniqueValues.Add(value);
+            }
+        }
+
+        public void RemoveValue(int value) {
+            UniqueValues.Remove(value);
+        }
+
+        public static MySet operator + (MySet lhs, MySet rhs) {            
+            var result = new MySet();
+            result.UniqueValues.AddRange(lhs.UniqueValues);
+
+            foreach (var value in rhs.UniqueValues) {
+                if (!result.UniqueValues.Contains(value)) {
+                    result.UniqueValues.Add(value);
+                }
+            }
+
+            return result;
+        }
+
+        public static MySet operator -(MySet lhs, MySet rhs) {
+            var result = new MySet();
+            result.UniqueValues.AddRange(lhs.UniqueValues);
+
+            foreach (var value in rhs.UniqueValues) {
+                if (result.UniqueValues.Contains(value)) {
+                    result.UniqueValues.Remove(value);
+                }
+            }
+
+            return result;
+        }
+
+        public static MySet operator *(MySet lhs, MySet rhs) {
+            var result = new MySet();
+
+            foreach (var lValue in lhs.UniqueValues) {
+                foreach (var rValue in rhs.UniqueValues) {
+                    if (lValue== rValue) {
+                        result.UniqueValues.Add(lValue);
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static bool operator ==(MySet lhs, MySet rhs) {
+            if (lhs.UniqueValues.Count!= rhs.UniqueValues.Count) {
+                return false;
+            }
+
+            foreach (var lValue in lhs.UniqueValues) {
+                foreach (var rValue in rhs.UniqueValues) {
+                    if (lValue != rValue) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static bool operator !=(MySet lhs, MySet rhs) {
+            return !(lhs == rhs);
+        }
+    }
+}
